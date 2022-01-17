b34f90f5719cfd1e7633c90ed4a20ca97af8ed1a
harri665 <harrison.d.martin@gmail.com>
2022-01-16 21:24:07 -0700

    added readme

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 
-from asyncio.windows_events import NULL
-from glob import glob
-from msilib.schema import File
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
